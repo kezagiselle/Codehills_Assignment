41ac269de25d6416cf75147738225195748634de
kezagiselle <kezagiselle776@gmail.com>
2025-12-30 10:37:40 +0200

    feat: setting up the springboot

---
+++ carManagement/src/test/java/com/example/carManagement/CarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
