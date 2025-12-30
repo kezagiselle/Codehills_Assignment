c614acdd7cbce6dd8ce0bf3a6996c52e5b89de3e
kezagiselle <kezagiselle776@gmail.com>
2025-12-30 10:51:18 +0200

    feat: finished creating the car model

--- carManagement/src/main/java/com/example/carManagement/models/Car.java
+++ carManagement/src/main/java/com/example/carManagement/models/Car.java
@@ -1,3 +1,26 @@
-public class car {
-    
+package com.example.carManagement.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+ 
+    private Long id;
+    private String brand;
+    private String model;
+    private int year;
+    private List<FuelEntry> fuelEntries = new ArrayList<>();
+
+       public Car(Long id, String brand, String model, int year) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public Long getId(){ return id;}
+    public String getBrand(){ return brand; }
+    public String getModel(){ return model; }
+    public int getYear(){ return year; }
+    public List<FuelEntry> getFuelEntries(){ return fuelEntries; }
 }
