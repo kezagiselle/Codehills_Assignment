051940f25c67db8bd78f92313980e2c5174366bb
kezagiselle <kezagiselle776@gmail.com>
2025-12-30 10:54:31 +0200

    feat: finished creating the fuel model

--- carManagement/src/main/java/com/example/carManagement/models/FuelEntry.java
+++ carManagement/src/main/java/com/example/carManagement/models/FuelEntry.java
@@ -1,3 +1,19 @@
+package com.example.carManagement.models;
+
+
 public class FuelEntry {
+    private double liters;
+    private double price;
+    private double odometer;
+
+    public FuelEntry(double liters, double price, double odometer) {
+        this.liters = liters;
+        this.price = price;
+        this.odometer = odometer;
+    }
     
+    public double getLiters(){ return liters;}
+    public double getPrice(){ return price;}
+    public double getOdometer(){ return odometer;}
 }
+
